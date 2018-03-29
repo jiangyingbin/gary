4e1949656b63d7007718a7223611831820b0883f
jiangyingbin <37890700+jiangyingbin@users.noreply.github.com>
2018-03-29 22:45:41 +0800

    Delete pbl.js

--- pbl.js
+++
@@ -1,9 +0,0 @@
-     $(function(){
-            	$("img").lazyload({effect: "fadeIn",failurelimit : 7,threshold: 30});
-            	$(window).scroll(function(){
-            	    /* 判断是否到达底部*/           
-            		if($(document).scrollTop() >= $(document).height() - $(window).height()){
-            	  	$('img').addClass('fl');            	  	           	  	
-            	  }
-            	})
-            })
